80e7bb24c5884d93b96608239fbbfd9593f1eb34
Niklas Sombert <niklas@ytvwld.de>
2017-10-27 21:36:18 +0200

    Define the variable data.

--- cast/cast.js
+++ cast/cast.js
@@ -4,7 +4,7 @@ var castReceiverManager = cast.receiver.CastReceiverManager.getInstance();
 
 var messageBus = castReceiverManager.getCastMessageBus('urn:x-cast:de.ytvwld.pixelcast');
 messageBus.onMessage = function(event) {
-  data = JSON.parse(event.data);
+  var data = JSON.parse(event.data);
   if(data["command"] == "draw") {
     draw(
       data["x"],
